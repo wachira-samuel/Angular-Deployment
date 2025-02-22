771e5d2a8f7db2ddab9ee16b2e2c5eab2157a37f
SAMWEL <wachirasamwel69@gmail.com>
2025-02-22 12:39:40 +0300

    First deployment

--- .github/actions/deployment/index.js
+++ .github/actions/deployment/index.js
@@ -1,23 +1,23 @@
-const core = require("@actions/core"); 
-const exec = require("@actions/exec");
+// const core = require("@actions/core");
+// const exec = require("@actions/exec");
 
-async function run() {
-  try {
-    const bucket = core.getInput("bucketName", { required: true });
-    const bucketRegion = core.getInput("bucketRegion", { required: true });
-    const distFiles = core.getInput("distFiles", { required: true });
+// async function run() {
+//   try {
+//     const bucket = core.getInput("bucketName", { required: true });
+//     const bucketRegion = core.getInput("bucketRegion", { required: true });
+//     const distFiles = core.getInput("distFiles", { required: true });
 
-    const s3Url = `s3://${bucket}`;
+//     const s3Url = `s3://${bucket}`;
 
-    await exec.exec(
-      `aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`
-    );
+//     await exec.exec(
+//       `aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`
+//     );
 
-    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-    core.setOutput("URL", websiteURL);
-    core.info(`${websiteURL}`);
-  } catch (error) {
-    core.setFailed(`${error.message}`);
-  }
-}
-run();
+//     const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+//     core.setOutput("URL", websiteURL);
+//     core.info(`${websiteURL}`);
+//   } catch (error) {
+//     core.setFailed(`${error.message}`);
+//   }
+// }
+// run();
